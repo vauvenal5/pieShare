0153288a12dbd6b5c0c14bd7670fbee5d0f7694a
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2013-12-12 16:03:29 +0100

    started with api's

---
+++ piePlate/src/main/java/org/pieTools/piePlate/service/cluster/ClusterService.java
@@ -0,0 +1,9 @@
+package org.pieTools.piePlate.service.cluster;
+
+import org.pieTools.piePlate.service.api.IClusterService;
+
+/**
+ * Created by vauvenal5 on 12/12/13.
+ */
+public class ClusterService implements IClusterService {
+}
