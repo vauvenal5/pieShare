da28ae0d2a41090efcea130ca1fefd6ea3e84d3a
Richard Leopold <richy.leopold@outlook.com>
2013-12-12 17:14:49 +0100

    added Spring

---
+++ pieUtilities/src/main/java/org/pieTools/pieUtilities/beanService/BeanService.java
@@ -0,0 +1,19 @@
+package org.pieTools.pieUtilities.beanService;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+
+public class BeanService implements IBeanService, ApplicationContextAware {
+    ApplicationContext context;
+
+    public void setApplicationContext(ApplicationContext context) {
+        this.context = context;
+    }
+
+    public <T> T getBean(Class<T> type) {
+        return context.getBean(type);
+    }
+
+}
