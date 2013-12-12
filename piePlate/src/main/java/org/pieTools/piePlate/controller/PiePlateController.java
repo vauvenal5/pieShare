99947a0e6a32c0c090a54ec781410ad85af44e19
Richard Leopold <richy.leopold@outlook.com>
2013-12-12 17:52:18 +0100

    implemented ICluster interface in Main Controller

--- piePlate/src/main/java/org/pieTools/piePlate/controller/PiePlateController.java
+++ piePlate/src/main/java/org/pieTools/piePlate/controller/PiePlateController.java
@@ -1,9 +1,11 @@
 package org.pieTools.piePlate.controller;
 
+import org.pieTools.piePlate.controller.api.ICluster;
+import org.pieTools.piePlate.controller.exception.ClusterException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class PiePlateController {
+public class PiePlateController implements ICluster {
 
     private ApplicationContext context = null;
 
@@ -12,7 +14,15 @@ public class PiePlateController {
     }
 
 
+    @Override
+    public void joinCluster(String cloudName) throws ClusterException {
 
+    }
+
+    @Override
+    public void leafCluster(String cloudName) throws ClusterException {
+
+    }
 }
 
 
