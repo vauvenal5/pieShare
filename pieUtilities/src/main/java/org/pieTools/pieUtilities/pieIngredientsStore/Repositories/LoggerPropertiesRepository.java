93b6b7b1df1444aea97fd5fab09516227ce82ad3
Richard Leopold <richy.leopold@outlook.com>
2014-01-10 13:41:07 +0100

    started implementing JPA Spring Data, Databse

---
+++ pieUtilities/src/main/java/org/pieTools/pieUtilities/pieIngredientsStore/Repositories/LoggerPropertiesRepository.java
@@ -0,0 +1,15 @@
+package org.pieTools.pieUtilities.pieIngredientsStore.repositories;
+
+import org.pieTools.pieUtilities.pieIngredientsStore.entities.LoggerPropertiesEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by Richard on 09.01.14.
+ */
+@Repository
+public interface LoggerPropertiesRepository extends JpaRepository<LoggerPropertiesEntity, Long> {
+	List<LoggerPropertiesEntity> findByPropertyname(String propertyname);
+}
---
+++ pieUtilities/src/main/test/java/org/pieTools/pieUtilities/pieIngredientsStoreTest/DatabaseTest.java
@@ -0,0 +1,54 @@
+package org.pieTools.pieUtilities.pieIngredientsStoreTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+import org.pieTools.pieUtilities.pieIngredientsStore.LoggerPropertiesDAO;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Properties;
+
+/**
+ * Created by Richard on 09.01.14.
+ */
+public class DatabaseTest {
+
+	@Before
+	public void startUp() {
+
+	}
+
+	@After
+	public void cleanUp() {
+
+	}
+
+	@Test
+	public void testLoggerPropertiesDAO() {
+		String propName = "TestLoggerProperties";
+		final AbstractApplicationContext context = new ClassPathXmlApplicationContext("applicationContextTest.xml");
+		context.registerShutdownHook();
+		LoggerPropertiesDAO loggerPropertiesDAO = context.getBean(LoggerPropertiesDAO.class);
+
+		Properties props = new Properties();
+
+		props.setProperty("log4j.rootLogger", "DEBUG, FILE");
+		props.setProperty("log4j.appender.FILE", "org.apache.log4j.FileAppender");
+		props.setProperty("log4j.appender.FILE.File", "log.out");
+		props.setProperty("log4j.appender.FILE.ImmediateFlush", "true");
+		props.setProperty("log4j.appender.FILE.Threshold", "debug");
+		props.setProperty("log4j.appender.FILE.Append", "false");
+		props.setProperty("log4j.appender.FILE.layout", "org.apache.log4j.PatternLayout");
+		props.setProperty("log4j.appender.FILE.layout.conversionPattern", "%m%n");
+
+		loggerPropertiesDAO.setNewProperty(props, propName);
+
+		Properties fromDB = loggerPropertiesDAO.getPropertyByName(propName);
+
+		Assert.assertEquals(fromDB.getProperty("log4j.rootLogger"), "DEBUG, FILE");
+
+	}
+
+}
