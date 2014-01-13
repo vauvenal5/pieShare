580d72590b12c8f47cd4658db667249a5d8878ed
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2014-01-13 11:29:36 +0100

    work in progress for cluster management

--- piePlate/src/main/java/org/pieTools/piePlate/service/cluster/api/IClusterService.java
+++ piePlate/src/main/java/org/pieTools/piePlate/service/cluster/api/IClusterService.java
@@ -1,21 +1,13 @@
 package org.pieTools.piePlate.service.cluster.api;
 
-import org.jgroups.JChannel;
 import org.jgroups.Message;
-import org.pieTools.piePlate.dto.PieMessage;
 import org.pieTools.piePlate.service.exception.ClusterServiceException;
 
 /**
  * Created by vauvenal5 on 12/12/13.
  */
 public interface IClusterService {
-    void connect(String clusterName) throws ClusterServiceException;
+    IClusterWrapper connect(String clusterName) throws ClusterServiceException;
 
     void handleMessage(Message msg);
-
-    void sendMessage(PieMessage msg) throws ClusterServiceException;
-
-    int getMembersCount();
-
-    boolean isConnectedToCluster();
 }
