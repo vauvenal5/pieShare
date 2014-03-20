5474ee87bad2e6546bd81fedb3edd4e35ba3c172
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2014-03-20 10:45:16 +0100

    add pieShareApp module

---
+++ pieShareApp/src/main/java/org/pieShare/pieShareApp/App.java
@@ -0,0 +1,15 @@
+package org.pieShare.pieShareApp;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by vauvenal5 on 3/20/14.
+ */
+public class App {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+
+    }
+}
---
+++ pieShareApp/src/main/java/org/pieShare/pieShareApp/controller/PieShareController.java
@@ -0,0 +1,7 @@
+package org.pieShare.pieShareApp.controller;
+
+/**
+ * Created by vauvenal5 on 3/20/14.
+ */
+public class PieShareController {
+}
