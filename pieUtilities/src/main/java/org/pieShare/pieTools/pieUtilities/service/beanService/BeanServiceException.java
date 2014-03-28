b910ab5533ca81af8d90f59c3a8043350e3d0543
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2014-03-28 20:37:14 +0100

    added unit tests for PieExecutor

---
+++ pieUtilities/src/main/java/org/pieShare/pieTools/pieUtilities/service/beanService/BeanServiceException.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.pieShare.pieTools.pieUtilities.service.beanService;
+
+/**
+ *
+ * @author Svetoslav
+ */
+public class BeanServiceException extends Exception {
+
+    /**
+     * Creates a new instance of <code>BeanServiceException</code> without
+     * detail message.
+     */
+    public BeanServiceException(Throwable ex) {
+        super(ex);
+    }
+
+    /**
+     * Constructs an instance of <code>BeanServiceException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public BeanServiceException(String msg) {
+        super(msg);
+    }
+}
