10ff80f2307086b2987d517a3e2287c0ebe6eb27
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2014-03-31 22:28:13 +0200

    working on command parser tests

--- pieCeption/src/main/java/org/pieShare/pieTools/pieCeption/service/core/PieCeptionService.java
+++ pieCeption/src/main/java/org/pieShare/pieTools/pieCeption/service/core/PieCeptionService.java
@@ -4,19 +4,18 @@ import javax.annotation.PostConstruct;
 import org.pieShare.pieTools.pieCeption.service.core.api.IConnectorService;
 import org.pieShare.pieTools.pieCeption.service.core.api.IPieCeptionService;
 import org.pieShare.pieTools.pieCeption.service.core.exception.PieCeptionServiceException;
-import org.pieShare.pieTools.pieCeption.service.core.exception.StartupServiceException;
 import org.pieShare.pieTools.piePlate.model.message.api.IPieMessage;
 import org.pieShare.pieTools.pieUtilities.service.commandParser.api.ICommandParserService;
 import org.pieShare.pieTools.pieUtilities.service.commandParser.exception.CommandParserServiceException;
 import org.pieShare.pieTools.pieUtilities.service.pieExecutorService.api.IExecutorService;
 import org.pieShare.pieTools.pieUtilities.service.pieExecutorService.api.IPieEvent;
+import org.pieShare.pieTools.pieUtilities.service.pieExecutorService.exception.PieExecutorServiceException;
 
 /**
  * Created by Svetoslav on 29.12.13.
  */
 public class PieCeptionService implements IPieCeptionService {
     private IConnectorService connectorService;
-    private ICommandParserService commandParserService;
     private IExecutorService executorService;
     private boolean isMaster;
 
@@ -27,20 +26,20 @@ public class PieCeptionService implements IPieCeptionService {
         this.connectorService = connectorService;
     }
 
-    public void setCommandParserService(ICommandParserService commandParserService){
-        this.commandParserService = commandParserService;
-    }
-
     @PostConstruct
     public void start() throws PieCeptionServiceException {
         this.connectorService.connectToMaster("pieShare");
         this.isMaster = !this.connectorService.isPieShareRunning();
     }
 
-    @Override
-    public void handlePieMessage(IPieMessage message) {
+    @Override 
+   public void handlePieMessage(IPieMessage message) {
         if(this.isMaster) {
-            this.executorService.handlePieEvent(message);
+            try {
+                this.executorService.handlePieEvent(message);
+            } catch(PieExecutorServiceException ex) {
+                //todo-sv: command doesn't exist therefore print usage!
+            }
             return;
         }
         
