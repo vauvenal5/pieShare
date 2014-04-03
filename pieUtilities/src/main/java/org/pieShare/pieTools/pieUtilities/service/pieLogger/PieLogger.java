2d84c08ed59e80c49b0ff2363dbdd24d4096fb3f
Richard Leopold <richy.leopold@outlook.com>
2014-04-03 19:06:00 +0200

    logger changes

--- pieUtilities/src/main/java/org/pieShare/pieTools/pieUtilities/service/pieLogger/PieLogger.java
+++ pieUtilities/src/main/java/org/pieShare/pieTools/pieUtilities/service/pieLogger/PieLogger.java
@@ -1,14 +1,18 @@
 package org.pieShare.pieTools.pieUtilities.service.pieLogger;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class PieLogger {
+public class PieLogger  {
 
-    private static Logger mainLogger;
+    private static final Logger mainLogger = LoggerFactory.getLogger(PieLogger.class);
 
+    
+    
     public PieLogger(Class clazz) {
-        mainLogger = LoggerFactory.getLogger(clazz);
+        //mainLogger = LoggerFactory.getLogger(clazz);
+	// PropertyConfigurator.configure("log4j.properties");
     }
 
     public void debug(String message) {
