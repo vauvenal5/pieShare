da68bac1ab45b7a925f16792048df11003a3a079
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2014-04-03 18:34:02 +0200

    fixed context bugs

--- pieShareApp/src/main/java/org/pieShare/pieShareApp/App.java
+++ pieShareApp/src/main/java/org/pieShare/pieShareApp/App.java
@@ -1,5 +1,6 @@
 package org.pieShare.pieShareApp;
 
+import org.pieShare.pieShareApp.service.PieShareService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,6 +10,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("pieShareApplicationContext.xml");
+        PieShareService service = (PieShareService)context.getBean("pieShareService");
+        service.start();
         //PieShareController controller = (PieShareController)context.getBean("pieShareController");
         //controller.run();
     }
