7b9fe8123ede420b6286a5c7cd85cf2d38a09aae
Richy <richard.leopold@piesystems.org>
2014-04-16 15:38:27 +0200

    changed compressor to work with byte[] instead streams

--- pieUtilities/src/main/java/org/pieShare/pieTools/pieUtilities/service/compressor/Compressor.java
+++ pieUtilities/src/main/java/org/pieShare/pieTools/pieUtilities/service/compressor/Compressor.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
+import java.util.zip.DeflaterInputStream;
 import java.util.zip.Inflater;
 import org.apache.commons.lang3.Validate;
 import org.pieShare.pieTools.pieUtilities.service.compressor.api.ICompressor;
@@ -23,118 +24,55 @@ public class Compressor implements ICompressor
 {
 
     private PieLogger logger = new PieLogger(Compressor.class);
-    private boolean isCompressJob = true;
-    private InputStream in;
-    private OutputStream out;
 
     @Override
-    public void compressStream(InputStream in, OutputStream out) throws IOException
+    public void compressStream(byte[] data, OutputStream out) throws IOException
     {
-        byte[] buff = new byte[1024];
         Deflater deflater = new Deflater();
-        int readBytes = 0;
 
-        while ((readBytes = in.read(buff)) != -1)
+        deflater.setInput(data);
+
+        deflater.finish();
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished())
         {
-            deflater.setInput(buff, 0, readBytes);
+            int count = deflater.deflate(buffer); // returns the generated code... index  
+            out.write(buffer, 0, count);
 
-            deflater.finish();
-            byte[] buffer = new byte[1024];
-            while (!deflater.finished())
-            {
-                int count = deflater.deflate(buffer); // returns the generated code... index  
-                out.write(buffer, 0, count);
-
-                if (deflater.needsInput())
-                {
-                    break;
-                }
-
-            }
         }
         deflater.reset();
         //deflater.end();
     }
 
     @Override
-    public void decompressStream(InputStream in, OutputStream out) throws IOException, DataFormatException
+    public void decompressStream(byte[] data, OutputStream out) throws IOException, DataFormatException
     {
         byte[] buff = new byte[1024];
-        int readBytes = 0;
-
+       
         Inflater inflater = new Inflater();
-        while ((readBytes = in.read(buff)) != -1)
+
+        inflater.setInput(data);
+
+        // ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!inflater.finished())
         {
-            inflater.setInput(buff, 0, readBytes);
+            int count = inflater.inflate(buffer);
+            out.write(buffer, 0, count);
 
-            // ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
-            byte[] buffer = new byte[1024];
-            while (!inflater.finished())
+            if (inflater.needsInput())
             {
-                int count = inflater.inflate(buffer);
-                out.write(buffer, 0, count);
+                break;
+            }
 
-                if (inflater.needsInput())
-                {
-                    break;
-                }
-
-                if (inflater.needsDictionary())
-                {
-                    //ToDo: Check Problem with speziel chars (sonderzeichen);
-                }
+            if (inflater.needsDictionary())
+            {
+                //ToDo: Check Problem with speziel chars (sonderzeichen);
             }
         }
+
         //inflater.end();
         inflater.reset();
     }
 
-    @Override
-    public void run()
-    {
-        Validate.notNull(out);
-        Validate.notNull(in);
-
-        try
-        {
-            if (isCompressJob)
-            {
-                compressStream(in, out);
-            }
-            else
-            {
-                decompressStream(in, out);
-            }
-        }
-        catch (IOException | DataFormatException ex)
-        {
-            logger.error("Error while compressing. Message: " + ex.getMessage());
-        }
-    }
-
-    @Override
-    public void setNewCompressJob(InputStream in, OutputStream out) throws IOException
-    {
-        Validate.notNull(out);
-        Validate.notNull(in);
-
-        in.available();
-
-        isCompressJob = true;
-        this.in = in;
-        this.out = out;
-    }
-
-    @Override
-    public void setNewDeCompressJob(InputStream in, OutputStream out) throws IOException
-    {
-        Validate.notNull(out);
-        Validate.notNull(in);
-
-        in.available();
-
-        isCompressJob = false;
-        this.in = in;
-        this.out = out;
-    }
 }
