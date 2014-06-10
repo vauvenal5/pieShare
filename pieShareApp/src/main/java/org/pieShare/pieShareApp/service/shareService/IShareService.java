be03851f06604e87eda2a44619d802c75bd65297
Svetoslav Videnov <svetoslav.videnov@piesystems.org>
2014-06-10 18:57:58 +0200

    added common shareService interface; started implementing bitTorrentService

---
+++ pieShareApp/src/main/java/org/pieShare/pieShareApp/service/shareService/IShareService.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.pieShare.pieShareApp.service.shareService;
+
+import java.io.File;
+import org.pieShare.pieShareApp.model.message.FileTransferMetaMessage;
+
+/**
+ *
+ * @author Svetoslav
+ */
+public interface IShareService {
+    
+    void shareFile(File file);
+    
+    void handleFile(FileTransferMetaMessage msg);
+    
+}
