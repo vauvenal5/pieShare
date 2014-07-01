c029aa3593c680616e79636109b8afac879db352
Richard Leopold <richy.leopold@outlook.com>
2014-07-01 19:15:51 +0200

    added comperer service, added RequestService Interface, fixed pieFile Equals function

---
+++ pieShareApp/src/main/java/org/pieShare/pieShareApp/service/comparerService/api/IComparerService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.pieShare.pieShareApp.service.comparerService.api;
+
+import java.io.IOException;
+import java.util.List;
+import org.pieShare.pieShareApp.service.comparerService.exceptions.FileConflictException;
+import org.pieShare.pieShareApp.service.fileService.PieFile;
+
+/**
+ *
+ * @author Richard
+ */
+public interface IComparerService {
+	public void comparePieFileList(List<PieFile> list) throws IOException, FileConflictException;
+	public boolean isPieFileDesired(PieFile remotePieFile) throws IOException, FileConflictException;
+}
