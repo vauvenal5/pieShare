e749e60db16f0908c364ab598c9c86ef2d822b6c
Svetoslav Videnov <svetoslav.videnov@piesystems.org>
2014-10-08 20:35:07 +0200

    work on fileService for syncing FileLists when joining cloud

---
+++ pieShareApp/src/main/java/org/pieShare/pieShareApp/model/message/FileListMessage.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.pieShare.pieShareApp.model.message;
+
+import java.util.List;
+import org.pieShare.pieShareApp.service.fileService.PieFile;
+import org.pieShare.pieTools.piePlate.model.message.HeaderMessage;
+
+/**
+ *
+ * @author Svetoslav
+ */
+public class FileListMessage extends HeaderMessage {
+	
+	private List<PieFile> fileList;
+	
+	public FileListMessage(List<PieFile> fileList) {
+		this.fileList = fileList;
+	}
+
+	public List<PieFile> getFileList() {
+		return fileList;
+	}
+}
