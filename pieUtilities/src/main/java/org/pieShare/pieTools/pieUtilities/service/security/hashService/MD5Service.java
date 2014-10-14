2a0a7675aa9811146ee9f54cc29988d700bb62ce
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2014-10-14 15:58:17 +0200

    fixed md5 to work concurrent

--- pieUtilities/src/main/java/org/pieShare/pieTools/pieUtilities/service/security/hashService/MD5Service.java
+++ pieUtilities/src/main/java/org/pieShare/pieTools/pieUtilities/service/security/hashService/MD5Service.java
@@ -19,20 +19,11 @@ import org.pieShare.pieTools.pieUtilities.service.security.IProviderService;
 public class MD5Service implements IHashService {
 
 	private static final PieLogger md5Logger = new PieLogger(MD5Service.class);
-	private MessageDigest messageDigest = null;
 
 	private IProviderService provider;
 
 	public void setProviderService(IProviderService service) {
 		this.provider = service;
-		
-		try {
-			messageDigest = MessageDigest.getInstance(this.provider.getFileHashAlorithm(), this.provider.getProviderName());
-		} catch (NoSuchAlgorithmException ex) {
-			md5Logger.error("Error in MD5 Hash Algorithm, this shold no happen. Message: " + ex.getMessage());
-		} catch (NoSuchProviderException ex) {
-			//todo: error handling
-		}
 	}
 
 	public MD5Service() {
@@ -41,26 +32,43 @@ public class MD5Service implements IHashService {
 
 	@Override
 	public byte[] hash(byte[] data) {
-		Validate.notNull(this.messageDigest);
+		MessageDigest messageDigest = this.getMessageDigest();
+		Validate.notNull(messageDigest);
 		messageDigest.update(data);
 		byte[] resultByte = messageDigest.digest();
-		this.messageDigest.reset();
+		messageDigest.reset();
 
 		return resultByte;
 	}
+	
+	private MessageDigest getMessageDigest() {
+		MessageDigest messageDigest = null;
+		
+		try {
+			messageDigest = MessageDigest.getInstance(this.provider.getFileHashAlorithm(), this.provider.getProviderName());
+		} catch (NoSuchAlgorithmException ex) {
+			md5Logger.error("Error in MD5 Hash Algorithm, this shold no happen. Message: " + ex.getMessage());
+		} catch (NoSuchProviderException ex) {
+			//todo: error handling
+		}
+		
+		return messageDigest;
+	}
 
 	@Override
-	public synchronized byte[] hashStream(InputStream stream) throws IOException {
+	public byte[] hashStream(InputStream stream) throws IOException {
+		MessageDigest messageDigest = this.getMessageDigest();
+		
 		byte[] buffer = new byte[1024];
 		int read = 0;
 		PieLogger.debug(this.getClass(),"testing logger");
 		while ((read = stream.read(buffer)) != -1) {
-			Validate.notNull(this.messageDigest);
+			Validate.notNull(messageDigest);
 			messageDigest.update(buffer, 0, read);
 		}
 
 		byte[] resultByte = messageDigest.digest();
-		this.messageDigest.reset();
+		messageDigest.reset();
 		return resultByte;
 	}
 
