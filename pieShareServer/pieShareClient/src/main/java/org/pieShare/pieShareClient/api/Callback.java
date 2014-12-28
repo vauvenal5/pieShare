1e36a1ad137cdf05b379e88381e0b63e6b952c77
unknown <RicLeo00@RICLEO20.omicron.at>
2014-12-28 20:46:20 +0100

    client fixes

---
+++ pieShareServer/pieShareClient/src/main/java/org/pieShare/pieShareClient/api/Callback.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pieShare.pieShareClient.api;
+
+/**
+ *
+ * @author RicLeo00
+ */
+public interface Callback {
+    void Handle();
+}
