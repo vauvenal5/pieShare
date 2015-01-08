6101582478b128802781b4a0ab5421150c136dbb
Svetoslav Videnov <svetoslav.videnov@piesystems.org>
2015-01-08 16:37:38 +0100

    - fixed timing issues in ITs - stabalized ITs - cleaned a little bit up but still work to do - add exception logging for all uncatched task based exceptions - deleted all autogenerated loggers

--- pieShareApp/src/main/java/org/pieShare/pieShareApp/model/message/FileListMessage.java
+++ pieShareApp/src/main/java/org/pieShare/pieShareApp/model/message/FileListMessage.java
@@ -6,25 +6,29 @@
 
 package org.pieShare.pieShareApp.model.message;
 
+import org.pieShare.pieShareApp.model.message.api.IFileListMessage;
 import java.util.List;
 import org.pieShare.pieShareApp.model.pieFile.PieFile;
 import org.pieShare.pieTools.piePlate.model.message.HeaderMessage;
+import org.pieShare.pieTools.piePlate.model.message.api.IEncryptedMessage;
 
 /**
  *
  * @author Svetoslav
  */
-public class FileListMessage extends HeaderMessage {
+public class FileListMessage extends HeaderMessage implements IFileListMessage {
 	
 	private List<PieFile> fileList;
 	
 	public FileListMessage() {
 	}
         
+	@Override
         public void setFileList(List<PieFile> list) {
             this.fileList = list;
         }
 
+	@Override
 	public List<PieFile> getFileList() {
 		return fileList;
 	}
