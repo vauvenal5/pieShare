c95f4dba7cd69608903a13ebe7146f6ccd9ebe2a
Richard Leopold <richy.leopold@outlook.com>
2015-01-19 12:05:38 +0100

    added IPieBaseMessage and check for MasterNode

--- piePlate/src/test/java/org/pieShare/pieTools/piePlate/service/JacksonSerializerTest.java
+++ piePlate/src/test/java/org/pieShare/pieTools/piePlate/service/JacksonSerializerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.pieShare.pieTools.piePlate.model.message.api.IBasePieMessage;
 import org.pieShare.pieTools.piePlate.model.message.api.IPieMessage;
 import org.pieShare.pieTools.piePlate.service.serializer.jacksonSerializer.JacksonSerializerService;
 import org.pieShare.pieTools.piePlate.service.unitTests.helper.TestMessage;
@@ -37,7 +38,7 @@ public class JacksonSerializerTest {
 
 	@Test
 	public void testDeserialize() throws Exception {
-		IPieMessage res = this.service.deserialize(this.testBuffer);
+		IBasePieMessage res = this.service.deserialize(this.testBuffer);
 
 		Assert.assertEquals(TestMessage.class, res.getClass());
 		Assert.assertEquals(this.msg.getMsg(), ((TestMessage) res).getMsg());
