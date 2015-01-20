dde7dade072abcfe9b362e7cb3eef69775e06a6c
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2015-01-20 19:32:30 +0100

    work in progress...

---
+++ pieShareApp/src/main/java/org/pieShare/pieShareApp/task/eventTasks/conflictTasks/ACheckConflictTask.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pieShare.pieShareApp.task.eventTasks.conflictTasks;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.pieShare.pieShareApp.model.pieFile.PieFile;
+import org.pieShare.pieShareApp.service.comparerService.api.ILocalFileCompareService;
+import org.pieShare.pieShareApp.service.comparerService.exceptions.FileConflictException;
+import org.pieShare.pieTools.piePlate.model.message.api.IPieMessage;
+import org.pieShare.pieTools.pieUtilities.service.pieExecutorService.api.event.IPieEvent;
+import org.pieShare.pieTools.pieUtilities.service.pieLogger.PieLogger;
+import org.pieShare.pieTools.pieUtilities.task.PieEventTaskBase;
+
+/**
+ *
+ * @author Svetoslav Videnov
+ */
+public abstract class ACheckConflictTask<T extends IPieMessage> extends PieEventTaskBase<T> {
+	
+	protected ILocalFileCompareService comparerService;
+	
+	public void setComparerService(ILocalFileCompareService comparerService) {
+		this.comparerService = comparerService;
+	}
+	
+	protected boolean isConflicted(PieFile file) {
+		try {
+			if(this.comparerService.compareWithLocalPieFile(file) == 1) {
+				return false;
+			}
+		} catch (IOException ex) {
+			PieLogger.error(this.getClass(), "Compare failed!", ex);
+		} catch (FileConflictException ex) {
+			PieLogger.error(this.getClass(), "Compare failed!", ex);
+		}
+		
+		return true;
+	}
+}
