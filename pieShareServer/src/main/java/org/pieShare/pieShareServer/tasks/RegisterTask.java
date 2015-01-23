a70fbf6d6b8fa6654e5a994c5229bfaea6224806
Richard Leopold <richy.leopold@outlook.com>
2015-01-23 15:27:22 +0100

    fixed server bug

--- pieShareServer/src/main/java/org/pieShare/pieShareServer/tasks/RegisterTask.java
+++ pieShareServer/src/main/java/org/pieShare/pieShareServer/tasks/RegisterTask.java
@@ -21,60 +21,60 @@ import org.pieShare.pieTools.pieUtilities.service.pieLogger.PieLogger;
  */
 public class RegisterTask implements IPieEventTask<RegisterMessage> {
 
-	private RegisterMessage msg;
-	private IUserPersistanceService userPersistanceService;
-	private ILoopHoleService loopHoleService;
+    private RegisterMessage msg;
+    private IUserPersistanceService userPersistanceService;
+    private ILoopHoleService loopHoleService;
 
-	public void setLoopHoleService(ILoopHoleService loopHoleService) {
-		this.loopHoleService = loopHoleService;
-	}
+    public void setLoopHoleService(ILoopHoleService loopHoleService) {
+        this.loopHoleService = loopHoleService;
+    }
 
-	public void setUserPersistanceService(IUserPersistanceService userPersistanceService) {
-		this.userPersistanceService = userPersistanceService;
-	}
+    public void setUserPersistanceService(IUserPersistanceService userPersistanceService) {
+        this.userPersistanceService = userPersistanceService;
+    }
 
-	@Override
-	public void setEvent(RegisterMessage msg) {
-		this.msg = msg;
-	}
+    @Override
+    public void setEvent(RegisterMessage msg) {
+        this.msg = msg;
+    }
 
-	@Override
-	public void run() {
+    @Override
+    public void run() {
 
-		HashMap<String, User> sameUsers = userPersistanceService.getUsersByName(msg.getName());
+        HashMap<String, User> sameUsers = userPersistanceService.getUsersByName(msg.getName());
 
-		User newUser = new User();
-		newUser.setId(msg.getId());
-		newUser.setName(msg.getName());
-		UdpAddress privateAddress = new UdpAddress();
-		privateAddress.setHost(msg.getPrivateHost());
-		privateAddress.setPort(msg.getPrivatePort());
-		newUser.setPrivateAddress(privateAddress);
-		newUser.setPublicAddress(msg.getSenderAddress());
-		userPersistanceService.addUser(newUser);
-                PieLogger.info(this.getClass(), String.format("User: %s registered sucessfully.", newUser.getName()));
+        User newUser = new User();
+        newUser.setId(msg.getId());
+        newUser.setName(msg.getName());
+        UdpAddress privateAddress = new UdpAddress();
+        privateAddress.setHost(msg.getPrivateHost());
+        privateAddress.setPort(msg.getPrivatePort());
+        
+        newUser.setPrivateAddress(privateAddress);
+        newUser.setPublicAddress(msg.getSenderAddress());
+        userPersistanceService.addUser(newUser);
+        PieLogger.info(this.getClass(), String.format("User: %s registered sucessfully.", newUser.getName()));
+
+        LoopHoleConnectionMessage connectionMessageToReceiver = new LoopHoleConnectionMessage();
+        connectionMessageToReceiver.setClientPrivateIP(newUser.getPrivateAddress().getHost());
+        connectionMessageToReceiver.setClientPrivatePort(newUser.getPrivateAddress().getPort());
+        connectionMessageToReceiver.setClientPublicIP(newUser.getPublicAddress().getHost());
+        connectionMessageToReceiver.setClientPublicPort(newUser.getPublicAddress().getPort());
+
+        for (User user : sameUsers.values()) {
+
+            if (!msg.getId().equals(user.getId())) {
                 
-                
-		LoopHoleConnectionMessage connectionMessageToReceiver = new LoopHoleConnectionMessage();
-		connectionMessageToReceiver.setClientPrivateIP(newUser.getPrivateAddress().getHost());
-		connectionMessageToReceiver.setClientPrivatePort(newUser.getPrivateAddress().getPort());
-		connectionMessageToReceiver.setClientPublicIP(newUser.getPublicAddress().getHost());
-		connectionMessageToReceiver.setClientPublicPort(newUser.getPublicAddress().getPort());
+                LoopHoleConnectionMessage connectionMessageToSender = new LoopHoleConnectionMessage();
+                connectionMessageToSender.setClientPrivateIP(user.getPrivateAddress().getHost());
+                connectionMessageToSender.setClientPrivatePort(user.getPrivateAddress().getPort());
+                connectionMessageToSender.setClientPublicIP(user.getPublicAddress().getHost());
+                connectionMessageToSender.setClientPublicPort(user.getPublicAddress().getPort());
 
-		for (User user : sameUsers.values()) {
+                loopHoleService.send(connectionMessageToSender, msg.getSenderAddress().getHost(), msg.getSenderAddress().getPort());
+            }
 
-			if (msg.getId().equals(user.getId())) {
-				continue;
-			}
-
-			LoopHoleConnectionMessage connectionMessageToSender = new LoopHoleConnectionMessage();
-			connectionMessageToSender.setClientPrivateIP(user.getPrivateAddress().getHost());
-			connectionMessageToSender.setClientPrivatePort(user.getPrivateAddress().getPort());
-			connectionMessageToSender.setClientPublicIP(user.getPublicAddress().getHost());
-			connectionMessageToSender.setClientPublicPort(user.getPublicAddress().getPort());
-
-			loopHoleService.send(connectionMessageToSender, msg.getSenderAddress().getHost(), msg.getSenderAddress().getPort());
-			loopHoleService.send(connectionMessageToReceiver, user.getPublicAddress().getHost(),user.getPublicAddress().getPort());
-		}
-	}
+            loopHoleService.send(connectionMessageToReceiver, user.getPublicAddress().getHost(), user.getPublicAddress().getPort());
+        }
+    }
 }
