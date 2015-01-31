4987efc4ba7461a269aa779c912dc127837b1531
Richard Leopold <richy.leopold@outlook.com>
2015-01-31 16:11:02 +0100

    added hack for awaiting loop hole

--- piePlate/src/main/java/org/pieShare/pieTools/piePlate/service/cluster/jgroupsCluster/ProtocolFactory.java
+++ piePlate/src/main/java/org/pieShare/pieTools/piePlate/service/cluster/jgroupsCluster/ProtocolFactory.java
@@ -95,7 +95,7 @@ public class ProtocolFactory {
 		
 		//todo: dynamic adding of sites when they join?
 		RELAY2 relay2 = new RELAY2();
-		relay2.site("test1");
+		relay2.site("test2");
 		relay2.relayMulticasts(true);
 		relay2.addSite("test1", site1);
 		relay2.addSite("test2", site2);
