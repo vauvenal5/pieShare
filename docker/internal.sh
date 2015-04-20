97383473e9fac0d64c7348c4123543b22930657a
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2015-04-20 19:38:23 +0200

    removed ut filter now loadTest runs only ones

--- docker/internal.sh
+++ docker/internal.sh
@@ -6,8 +6,8 @@ git clean -fd
 # Dtest is set to force skiping the unit tests by not finding any
 # DfailIfNoTests this is disabled to not fail after no unit tests where found
 # Dit.test sets the loadTest to be executed
-env LTTYPE=$1 mvn clean verify -Dtest=LoadTestIT#doNothing -Dit.test=LoadTestIT -DfailIfNoTests=false
-while true;
-do
-	sleep 1000
-done
+env LTTYPE=$1 mvn clean verify -Dit.test=LoadTestIT -DfailIfNoTests=false
+#while true;
+#do
+#	sleep 1000
+#done
