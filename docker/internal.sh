10d26c6d022e4b5af0799abf2d3552548458f150
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2015-04-20 19:16:03 +0200

    added doNothing test for sureFire plugin

--- docker/internal.sh
+++ docker/internal.sh
@@ -6,7 +6,7 @@ git clean -fd
 # Dtest is set to force skiping the unit tests by not finding any
 # DfailIfNoTests this is disabled to not fail after no unit tests where found
 # Dit.test sets the loadTest to be executed
-env LTTYPE=$1 mvn clean verify -Dtest=LoadTestIT -Dit.test=LoadTestIT -DfailIfNoTests=false
+env LTTYPE=$1 mvn clean verify -Dtest=LoadTestIT#doNothing -Dit.test=LoadTestIT -DfailIfNoTests=false
 while true;
 do
 	sleep 1000
