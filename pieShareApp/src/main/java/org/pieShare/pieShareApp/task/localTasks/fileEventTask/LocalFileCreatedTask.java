0d34620d708ecaaeb28fb9d013112780f46959b1
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2015-05-19 13:56:45 +0200

    refactored newFileMessage

--- pieShareApp/src/main/java/org/pieShare/pieShareApp/task/localTasks/fileEventTask/LocalFileCreatedTask.java
+++ pieShareApp/src/main/java/org/pieShare/pieShareApp/task/localTasks/fileEventTask/LocalFileCreatedTask.java
@@ -9,7 +9,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import org.pieShare.pieShareApp.model.message.api.INewFileMessage;
+import org.pieShare.pieShareApp.model.message.api.IFileCreatedMessage;
 import org.pieShare.pieShareApp.model.pieFile.PieFile;
 import org.pieShare.pieShareApp.service.fileService.api.IFileService;
 import org.pieShare.pieTools.pieUtilities.service.pieLogger.PieLogger;
@@ -35,7 +35,7 @@ public class LocalFileCreatedTask extends ALocalFileEventTask {
 				return;
 			}
 			
-			INewFileMessage msg = this.messageFactoryService.getNewFileMessage();
+			IFileCreatedMessage msg = this.messageFactoryService.getNewFileMessage();
 			
 			this.historyService.syncPieFileWithDb(pieFile);
 			
