7e407374eacf311ed037dafed57d71ea6352a83f
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2015-05-22 17:28:07 +0200

    debugging output

--- pieShareAppIntegrationTests/src/test/resources/docker/internal.sh
+++ pieShareAppIntegrationTests/src/test/resources/docker/internal.sh
@@ -4,6 +4,8 @@ git pull
 # Dtest is set to force skiping the unit tests by not finding any
 # DfailIfNoTests this is disabled to not fail after no unit tests where found
 # Dit.test sets the loadTest to be executed
+echo $1
+echo $2
 env LTTYPE=$1 LTFILES=$2 mvn clean verify -Dit.test=LoadTestIT -DfailIfNoTests=false
 #while true;
 #do
