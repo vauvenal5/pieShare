92a3bc0d1387dbd89c94d713bc2f672b282645ec
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2015-06-06 18:00:21 +0200

    experementing with the treshold

--- pieShareApp/src/main/java/org/pieShare/pieShareApp/task/localTasks/TorrentTask.java
+++ pieShareApp/src/main/java/org/pieShare/pieShareApp/task/localTasks/TorrentTask.java
@@ -116,7 +116,7 @@ public class TorrentTask extends AMessageSendingTask implements IShutdownableSer
 			boolean loopDone = false;
 			long lastAmount = 0;
 			int errorSeconds = 0;
-			int errorThreshold = 15;
+			int errorThreshold = 10;
 			boolean errorState = false;
 
 			while (!Client.ClientState.DONE.equals(client.getState()) && !loopDone) {
