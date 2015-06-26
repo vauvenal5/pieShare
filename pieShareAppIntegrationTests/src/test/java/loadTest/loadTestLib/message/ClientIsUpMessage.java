11cba22b8fd2d32b3fa186ab9cf61511cfd359c9
Richard <richy.leopold@outlook.com>
2015-06-26 14:02:44 +0000

    added Client Is up message

---
+++ pieShareAppIntegrationTests/src/test/java/loadTest/loadTestLib/message/ClientIsUpMessage.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loadTest.loadTestLib.message;
+
+import org.pieShare.pieTools.piePlate.model.message.AClusterMessage;
+import org.pieShare.pieTools.piePlate.model.message.api.IEncryptedMessage;
+
+/**
+ *
+ * @author richy
+ */
+public class ClientIsUpMessage extends AClusterMessage implements IEncryptedMessage {
+    
+}
