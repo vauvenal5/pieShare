86004541228aa479e39c7f6738c97dd1930f0aaf
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2015-06-27 15:14:30 +0200

    prepared loadTest for cloud

--- pieShareAppIntegrationTests/src/test/java/loadTest/loadTestLib/LUtil.java
+++ pieShareAppIntegrationTests/src/test/java/loadTest/loadTestLib/LUtil.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
