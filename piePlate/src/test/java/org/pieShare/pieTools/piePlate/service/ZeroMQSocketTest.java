0713db5abbc908f2e0e9359870114eb923705201
Paul Mueller <paul.otto.mueller@gmail.com>
2015-11-06 19:08:09 +0100

    #397 added tests for multiple senders one receiver and one sender multiple receiver, quick fix for dealer round robin problem

--- piePlate/src/test/java/org/pieShare/pieTools/piePlate/service/ZeroMQSocketTest.java
+++ piePlate/src/test/java/org/pieShare/pieTools/piePlate/service/ZeroMQSocketTest.java
@@ -60,4 +60,102 @@ public class ZeroMQSocketTest {
         
         assertArrayEquals(messageSend, messageRecv);
     }
+    
+    @Test
+    public void testMultipleSenders() {
+        Runnable r = new Runnable(){
+            @Override
+            public void run() {
+                PieDealer dealer = new PieDealer();
+                try {
+                    InetAddress routerAddress = InetAddress.getLocalHost();
+                    
+                    dealer.connect(routerAddress, 9000);
+
+                } catch (UnknownHostException e) {
+
+                }
+
+                byte[] messageSend = new byte[]{1,2,3,4,5,6,7,8,9,10};
+                dealer.send(messageSend);
+
+                dealer.close();
+            }
+        };
+        
+        byte[] messageSend = new byte[]{1,2,3,4,5,6,7,8,9,10};
+        byte[] messageRecv = null;
+        
+        PieRouter router = new PieRouter();
+        try {
+            InetAddress routerAddress = InetAddress.getLocalHost();
+            router.bind(routerAddress, 9000);
+            
+            for (int i = 0; i < 5; i++) {
+                Thread n = new Thread(r);
+                n.start();
+            }
+            
+            for (int i = 0; i < 5; i++) {
+                messageRecv = router.receive();
+                assertArrayEquals(messageSend, messageRecv);
+            }
+            
+            router.close();
+            
+        } catch (UnknownHostException e) {
+            System.out.println(e.toString());
+        }
+    }
+    
+    @Test
+    public void testSendMultipleRecv() {
+        Thread n = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                PieDealer dealer = new PieDealer();
+                try {
+                    InetAddress routerAddress = InetAddress.getLocalHost();
+                    
+                    dealer.connect(routerAddress, 9000);
+                    dealer.connect(routerAddress, 9001);
+
+                } catch (UnknownHostException e) {
+
+                }
+
+                byte[] messageSend = new byte[]{1,2,3,4,5,6,7,8,9,10};
+                dealer.send(messageSend);
+
+                dealer.close();
+            }
+        });
+        
+        byte[] messageSend = new byte[]{1,2,3,4,5,6,7,8,9,10};
+        byte[] messageRecv = null;
+        
+        PieRouter router = new PieRouter();
+        PieRouter router2 = new PieRouter();
+        
+        try {
+            InetAddress routerAddress = InetAddress.getLocalHost();
+            router.bind(routerAddress, 9000);
+            router2.bind(routerAddress, 9001);
+            
+            n.start();
+            
+            messageRecv = router.receive();
+            assertArrayEquals(messageSend, messageRecv);
+            
+            messageRecv = router2.receive();
+            assertArrayEquals(messageSend, messageRecv);
+            
+            router.close();
+            router2.close();
+            
+        } catch (UnknownHostException e) {
+            System.out.println(e.toString());
+        }
+    }        
+
 }
