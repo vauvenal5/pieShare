dd6c20c5bd4b496997a9f6f1029f0f1a478712ab
Paul Mueller <paul.otto.mueller@gmail.com>
2015-11-06 13:41:20 +0100

    #397 implementation fo PieDealer and PieRouter

--- piePlate/src/main/java/org/pieShare/pieTools/piePlate/service/cluster/zeroMqCluster/socket/PieDealer.java
+++ piePlate/src/main/java/org/pieShare/pieTools/piePlate/service/cluster/zeroMqCluster/socket/PieDealer.java
@@ -7,31 +7,55 @@ package org.pieShare.pieTools.piePlate.service.cluster.zeroMqCluster.socket;
 
 import java.net.InetAddress;
 import org.pieShare.pieTools.piePlate.service.cluster.zeroMqCluster.socket.api.IPieDealer;
+import org.pieShare.pieTools.pieUtilities.service.pieLogger.PieLogger;
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQException;
 
 /**
  *
  * @author Paul
  */
 public class PieDealer implements IPieDealer {
+     
+    private ZMQ.Socket dealer;
+    private ZeroMQUtilsService utils;
+    
+    public PieDealer()
+    {
+        ZMQ.Context context = ZMQ.context(1);
+        this.dealer = context.socket(ZMQ.DEALER);
+        this.utils = new ZeroMQUtilsService();
+    }
 
     @Override
     public boolean connect(InetAddress address) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+            PieLogger.trace(PieDealer.class, "Connecting to {0}", utils.buildConnectionString(address));
+            dealer.connect(utils.buildConnectionString(address));
+            return true;
+        }catch(ZMQException e){
+            PieLogger.error(PieDealer.class, "Connection failed {0}", e);
+            return false;
+        }
     }
 
     @Override
     public void disconnect(InetAddress address) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PieLogger.trace(PieDealer.class, "Connecting to {0}", utils.buildConnectionString(address));
+        dealer.disconnect(utils.buildConnectionString(address));
     }
 
     @Override
     public void close() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        dealer.close();
     }
 
     @Override
     public void send(byte[] message) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+            dealer.send(message, ZMQ.NOBLOCK);
+        }catch(ZMQException e){
+            PieLogger.error(PieDealer.class, "Message send error: {0}", e);
+        }
     }
-    
-}
+ }
