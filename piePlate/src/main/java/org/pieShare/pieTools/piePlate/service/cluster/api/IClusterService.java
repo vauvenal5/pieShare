4f98fe2dea9e7eca9e0aa00b3e203cb9d1e63a21
Paul Mueller <paul.otto.mueller@gmail.com>
2015-11-20 18:56:49 +0100

    #397 removed unused function from IClusterService interface

--- piePlate/src/main/java/org/pieShare/pieTools/piePlate/service/cluster/api/IClusterService.java
+++ piePlate/src/main/java/org/pieShare/pieTools/piePlate/service/cluster/api/IClusterService.java
@@ -2,14 +2,11 @@ package org.pieShare.pieTools.piePlate.service.cluster.api;
 
 import java.util.List;
 import org.pieShare.pieTools.piePlate.model.message.api.IClusterMessage;
-import org.pieShare.pieTools.piePlate.model.serializer.jacksonSerializer.JGroupsPieAddress;
 import org.pieShare.pieTools.piePlate.service.channel.api.IIncomingChannel;
 import org.pieShare.pieTools.piePlate.service.channel.api.IOutgoingChannel;
-import org.pieShare.pieTools.piePlate.service.channel.api.ITwoWayChannel;
 import org.pieShare.pieTools.piePlate.service.cluster.event.ClusterRemovedEvent;
 import org.pieShare.pieTools.piePlate.service.cluster.event.IClusterRemovedListener;
 import org.pieShare.pieTools.piePlate.service.cluster.exception.ClusterServiceException;
-import org.pieShare.pieTools.pieUtilities.model.EncryptedPassword;
 import org.pieShare.pieTools.pieUtilities.service.eventBase.IEventBase;
 
 /**
@@ -26,8 +23,6 @@ public interface IClusterService {
 	void disconnect() throws ClusterServiceException;
 
 	void sendMessage(IClusterMessage msg) throws ClusterServiceException;
-
-	int getMembersCount();
 
 	boolean isConnectedToCluster();
 	
