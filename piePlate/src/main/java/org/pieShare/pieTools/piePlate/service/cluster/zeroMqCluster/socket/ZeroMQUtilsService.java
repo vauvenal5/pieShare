52195ea6b9a2172a24694f11e0edb026a96c0e4c
Svetoslav Videnov <e1025844@student.tuwien.ac.at>
2015-12-13 20:16:35 +0100

    fixing router binding + started commenting mac behaviour #2936

--- piePlate/src/main/java/org/pieShare/pieTools/piePlate/service/cluster/zeroMqCluster/socket/ZeroMQUtilsService.java
+++ piePlate/src/main/java/org/pieShare/pieTools/piePlate/service/cluster/zeroMqCluster/socket/ZeroMQUtilsService.java
@@ -3,14 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.pieShare.pieTools.piePlate.service.cluster.zeroMqCluster.socket;
 
 import java.net.InetAddress;
 
 public class ZeroMQUtilsService {
-    public String buildConnectionString(InetAddress address, int port){
-        String connectionString = String.format("tcp://%s:%s", address.getHostAddress(), port);
-        return connectionString;
-    }
+
+	public String buildConnectionString(InetAddress address, int port) {
+		return this.buildConnectionString(address.getHostAddress(), port);
+	}
+
+	public String buildConnectionString(String address, int port) {
+		String connectionString = String.format("tcp://%s:%s", address, port);
+		return connectionString;
+	}
 }
