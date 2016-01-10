78cf08c579f0d99c6b37473260afcdfe3a166502
Daniela Köhle <e1027648@student.tuwien.ac.at>
2016-01-10 18:21:18 +0100

    comment interfaces

--- pieShareApp/src/main/java/org/pieShare/pieShareApp/model/entities/api/IPieFolderEntity.java
+++ pieShareApp/src/main/java/org/pieShare/pieShareApp/model/entities/api/IPieFolderEntity.java
@@ -13,8 +13,16 @@ package org.pieShare.pieShareApp.model.entities.api;
  */
 public interface IPieFolderEntity extends IBaseEntity{
     
-    //holds info whether there are changes and an according msg has already been sent
+    /**
+     * Holds info whether a folder is already synced to DB.
+     * @return true if file is in DB
+     */
     boolean isSynced();
+    
+    /**
+     * Set whether a folder is in the DB
+     * @param synced true for file in DB, false otherwise.
+     */
     void setSynced(boolean synced);
     
     //ID for the object
@@ -23,17 +31,45 @@ public interface IPieFolderEntity extends IBaseEntity{
     //ID for the object
     //void setAbsoluteWorkingPath(String absoluteWorkingPath);
 
+    /**
+     * States if the local folder is deleted. 
+     * @return true if the local file is deleted, false if it still exits.
+     */
     boolean isDeleted();
     
+    /**
+     * Set the deleted state of a folder. Deleted means that the local folder doesn't exist anymore.
+     * @param deleted true for a deleted folder, false for a still existing one.
+     */
     void setDeleted(boolean deleted);
     
     //ID
+    /**
+     * The relative path of a folder from the working directory as String.
+     * (including the name)
+     * Is also the ID
+     * @return the relative path of a folder
+     */
     String getRelativeFolderPath();
     
+    /**
+     * Set the relative path of a folder from the working directory as String.
+     * (including the name)
+     * IS also the ID
+     * @param relativeFolderPath the relative path of a folder = ID
+     */
     void setRelativeFolderPath(String relativeFolderPath);
     
+    /**
+     * The name of the folder
+     * @return folderName
+     */
     String getFolderName();
     
+    /**
+     * Set the name of a folder
+     * @param folderName 
+     */
     void setFolderName (String folderName);
     
     
