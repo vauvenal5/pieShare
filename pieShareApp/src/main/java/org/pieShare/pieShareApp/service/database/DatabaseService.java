67d5d7056b74e68956915d7a280537562ff0d950
richy1989 <richy.leopold@outlook.com>
2016-01-24 15:28:35 +0100

    fix

--- pieShareApp/src/main/java/org/pieShare/pieShareApp/service/database/DatabaseService.java
+++ pieShareApp/src/main/java/org/pieShare/pieShareApp/service/database/DatabaseService.java
@@ -29,251 +29,246 @@ import org.pieShare.pieTools.pieUtilities.service.pieLogger.PieLogger;
  */
 public class DatabaseService implements IDatabaseService {
 
-    private IModelEntityConverterService modelEntityConverterService;
-    private PieFilderDBService pieFilderDBService;
+	private IModelEntityConverterService modelEntityConverterService;
+	private PieFilderDBService pieFilderDBService;
 
-    private PieUserDAO pieUserDAO;
-    private FileFilterDAO fileFilterDAO;
+	private PieUserDAO pieUserDAO;
+	private FileFilterDAO fileFilterDAO;
 
-    public void setPieUserDAO(PieUserDAO pieUserDAO) {
-        this.pieUserDAO = pieUserDAO;
-    }
+	public void setPieUserDAO(PieUserDAO pieUserDAO) {
+		this.pieUserDAO = pieUserDAO;
+	}
 
-    public void setFileFilterDAO(FileFilterDAO fileFilterDAO) {
-        this.fileFilterDAO = fileFilterDAO;
-    }
+	public void setFileFilterDAO(FileFilterDAO fileFilterDAO) {
+		this.fileFilterDAO = fileFilterDAO;
+	}
 
-    public DatabaseService() {
-    }
+	public DatabaseService() {
+	}
 
-    public void setPieFilderDBService(PieFilderDBService pieFilderDBService) {
-        this.pieFilderDBService = pieFilderDBService;
-    }
+	public void setPieFilderDBService(PieFilderDBService pieFilderDBService) {
+		this.pieFilderDBService = pieFilderDBService;
+	}
 
-    @Override
-    public void setConverterService(IModelEntityConverterService converter) {
-        this.modelEntityConverterService = converter;
-    }
+	@Override
+	public void setConverterService(IModelEntityConverterService converter) {
+		this.modelEntityConverterService = converter;
+	}
 
-    @Override
-    public void persistPieUser(PieUser model) {
-        try {
-            pieUserDAO.savePieUser(modelEntityConverterService.convertToEntity(model));
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error Persisting PieUser", ex);
-        }
-    }
+	@Override
+	public void persistPieUser(PieUser model) {
+		try {
+			pieUserDAO.savePieUser(modelEntityConverterService.convertToEntity(model));
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error Persisting PieUser", ex);
+		}
+	}
 
-    @Override
-    public List<PieUser> findAllPieUser() {
+	@Override
+	public List<PieUser> findAllPieUser() {
+		ArrayList<PieUser> models = new ArrayList<>();
+		try {
+			for (PieUserEntity en : pieUserDAO.findAllPieUsers()) {
+				models.add(modelEntityConverterService.convertFromEntity(en));
+			}
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error find all PieUsers", ex);
+		}
 
-        try {
-            ArrayList<PieUser> models = new ArrayList<>();
-            for (PieUserEntity en : pieUserDAO.findAllPieUsers()) {
-                try {
-                    models.add(modelEntityConverterService.convertFromEntity(en));
-                } catch (Exception ex) {
-                    PieLogger.error(this.getClass(), "Error find all PieUsers", ex);
-                }
-            }
+		return models;
+	}
 
-            return models;
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error Persistung PieUser", ex);
-        }
-        return null;
-    }
+	@Override
+	public void removePieUser(PieUser user) {
+		PieUserEntity ent;
+		ent = modelEntityConverterService.convertToEntity(user);
+		try {
+			pieUserDAO.deletePieUser(ent);
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error Deleting PieUser", ex);
+		}
+	}
 
-    @Override
-    public void removePieUser(PieUser user) {
-        PieUserEntity ent;
-        ent = modelEntityConverterService.convertToEntity(user);
-        try {
-            pieUserDAO.deletePieUser(ent);
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error Deleting PieUser", ex);
-        }
-    }
+	@Override
+	public void mergePieUser(PieUser user) {
+		PieUserEntity entity;
+		entity = modelEntityConverterService.convertToEntity(user);
+		try {
+			if (pieUserDAO.findPieUserById(user.getCloudName()) != null) {
+				pieUserDAO.updatePieUser(entity);
+			} else {
+				pieUserDAO.savePieUser(entity);
+			}
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error Merging PieUser", ex);
+		}
+	}
 
-    @Override
-    public void mergePieUser(PieUser user) {
-        PieUserEntity entity;
-        entity = modelEntityConverterService.convertToEntity(user);
-        try {
-            if (pieUserDAO.findPieUserById(user.getCloudName()) != null) {
-                pieUserDAO.updatePieUser(entity);
-            } else {
-                pieUserDAO.savePieUser(entity);
-            }
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error Merging PieUser", ex);
-        }
-    }
+	@Override
+	public void persistFileFilter(IFilter filter) {
+		try {
+			fileFilterDAO.saveFilter((FilterEntity) filter);
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error Persisting File Filter", ex);
+		}
+	}
 
-    @Override
-    public void persistFileFilter(IFilter filter) {
-        try {
-            fileFilterDAO.saveFilter((FilterEntity) filter);
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error Persisting File Filter", ex);
-        }
-    }
+	@Override
+	public void removeFileFilter(IFilter filter) {
+		try {
+			fileFilterDAO.deleteFilter((FilterEntity) filter);
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error Deleting File Filter", ex);
+		}
+	}
 
-    @Override
-    public void removeFileFilter(IFilter filter) {
-        try {
-            fileFilterDAO.deleteFilter((FilterEntity) filter);
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error Deleting File Filter", ex);
-        }
-    }
+	@Override
+	public List<IFilter> findAllFilters() {
 
-    @Override
-    public List<IFilter> findAllFilters() {
+		List<IFilter> filters = new ArrayList<>();
 
-        List<IFilter> filters = new ArrayList<>();
+		try {
+			for (FilterEntity ent : fileFilterDAO.findAllFilter()) {
+				filters.add(modelEntityConverterService.convertFromEntity(ent));
+			}
 
-        try {
-            for (FilterEntity ent : fileFilterDAO.findAllFilter()) {
-                filters.add(modelEntityConverterService.convertFromEntity(ent));
-            }
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error Find All File Filters", ex);
+		}
+		return filters;
+	}
 
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error Find All File Filters", ex);
-        }
-        return filters;
-    }
+	@Override
+	public PieFile findPieFileByRelativeFilePath(PieFile file) {
+		try {
+			return this.pieFilderDBService.findFileByRelativePath(file.getRelativePath());
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error Finding one PieFile", ex);
+		}
+		return null;
+	}
 
-    @Override
-    public PieFile findPieFileByRelativeFilePath(PieFile file) {
-        try {
-            return this.pieFilderDBService.findFileByRelativePath(file.getRelativePath());
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error Finding one PieFile", ex);
-        }
-        return null;
-    }
+	@Override
+	public List<PieFile> findPieFileByHash(byte[] hash) {
+		try {
+			return pieFilderDBService.findPieFileByHash(hash);
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error finding all PieFiles by MD5!", ex);
+		}
+		return new ArrayList<>();
+	}
 
-    @Override
-    public List<PieFile> findPieFileByHash(byte[] hash) {
-        try {
-            return pieFilderDBService.findPieFileByHash(hash);
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error finding all PieFiles by MD5!", ex);        }
-        return new ArrayList<>();
-    }
+	@Override
+	public void mergePieFile(PieFile file) {
+		try {
+			pieFilderDBService.mergePieFile(file);
+		} catch (SQLException | IOException ex) {
+			PieLogger.error(this.getClass(), "Error Updating PieFile", ex);
+		}
+	}
 
-    @Override
-    public void mergePieFile(PieFile file) {
-        try {
-            pieFilderDBService.mergePieFile(file);
-        } catch (SQLException | IOException ex) {
-            PieLogger.error(this.getClass(), "Error Updating PieFile", ex);
-        }
-    }
+	@Override
+	public void persistPieFile(PieFile file) {
+		try {
+			pieFilderDBService.persistPieFile(file);
+		} catch (SQLException | IOException ex) {
+			PieLogger.error(this.getClass(), "Error Persisting PieFile", ex);
+		}
+	}
 
-    @Override
-    public void persistPieFile(PieFile file) {
-        try {
-            pieFilderDBService.persistPieFile(file);
-        } catch (SQLException | IOException ex) {
-            PieLogger.error(this.getClass(), "Error Persisting PieFile", ex);
-        }
-    }
+	@Override
+	public List<PieFile> findAllUnsyncedPieFiles() {
+		try {
+			return pieFilderDBService.findAllUnsyncedPieFiles();
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error findAllUnsyncedPieFiles", ex);
+		}
+		return new ArrayList<>();
+	}
 
-    @Override
-    public List<PieFile> findAllUnsyncedPieFiles() {
-        try {
-            return pieFilderDBService.findAllUnsyncedPieFiles();
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error findAllUnsyncedPieFiles", ex);
-        }
-        return new ArrayList<>();
-    }
+	@Override
+	public void resetAllPieFileSynchedFlags() {
+		try {
+			pieFilderDBService.resetAllPieFileSynchedFlags();
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error reseting all Synched Flags", ex);
+		}
+	}
 
-    @Override
-    public void resetAllPieFileSynchedFlags() {
-        try {
-            pieFilderDBService.resetAllPieFileSynchedFlags();
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error reseting all Synched Flags", ex);
-        }
-    }
+	@Override
+	public List<PieFile> findAllPieFiles() {
+		try {
+			return pieFilderDBService.findAllPieFiles();
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error finding all PieFiles!", ex);
+		}
+		return new ArrayList<>();
+	}
 
-    @Override
-    public List<PieFile> findAllPieFiles() {
-        try {
-            return pieFilderDBService.findAllPieFiles();
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error finding all PieFiles!", ex);
-        }
-        return new ArrayList<>();
-    }
+	@Override
+	public void persistPieFolder(PieFolder folder) {
+		try {
+			pieFilderDBService.persistPieFolder(folder);
+		} catch (SQLException | IOException ex) {
+			PieLogger.error(this.getClass(), "Error Persisting PieFolder!", ex);
+		}
+	}
 
-    @Override
-    public void persistPieFolder(PieFolder folder) {
-        try {
-            pieFilderDBService.persistPieFolder(folder);
-        } catch (SQLException | IOException ex) {
-            PieLogger.error(this.getClass(), "Error Persisting PieFolder!", ex);
-        }
-    }
+	@Override
+	public void mergePieFolder(PieFolder folder) {
+		try {
+			pieFilderDBService.mergePieFolder(folder);
+		} catch (SQLException | IOException ex) {
+			PieLogger.error(this.getClass(), "Error Merging PieFolder!", ex);
+		}
+	}
 
-    @Override
-    public void mergePieFolder(PieFolder folder) {
-        try {
-            pieFilderDBService.mergePieFolder(folder);
-        } catch (SQLException | IOException ex) {
-            PieLogger.error(this.getClass(), "Error Merging PieFolder!", ex);
-        }
-    }
+	@Override
+	public PieFolder findPieFolderByRelativeFilePath(PieFolder folder) {
+		try {
+			return pieFilderDBService.findFolderByRelativePath(folder.getRelativePath());
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error Finding PieFolder!", ex);
+		}
+		return null;
+	}
 
-    @Override
-    public PieFolder findPieFolderByRelativeFilePath(PieFolder folder) {
-        try {
-            return pieFilderDBService.findFolderByRelativePath(folder.getRelativePath());
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error Finding PieFolder!", ex);
-        }
-        return null;
-    }
+	@Override
+	public List<PieFolder> findAllUnsyncedPieFolders() {
+		try {
+			return pieFilderDBService.findAllUnsyncedPieFolders();
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error findAllUnsynced PieFolders!", ex);
+		}
+		return new ArrayList<>();
+	}
 
-    @Override
-    public List<PieFolder> findAllUnsyncedPieFolders() {
-        try {
-            return pieFilderDBService.findAllUnsyncedPieFolders();
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error findAllUnsynced PieFolders!", ex);
-        }
-        return new ArrayList<>();
-    }
+	@Override
+	public List<PieFolder> findAllPieFolders() {
 
-    @Override
-    public List<PieFolder> findAllPieFolders() {
+		try {
+			return pieFilderDBService.findAllPieFolders();
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error find all PieFolders!", ex);
+		}
+		return new ArrayList<>();
+	}
 
-        try {
-            return pieFilderDBService.findAllPieFolders();
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error find all PieFolders!", ex);
-        }
-        return new ArrayList<>();
-    }
+	@Override
+	public void resetAllPieFolderSyncedFlags() {
+		try {
+			pieFilderDBService.resetAllPieFolderSyncedFlags();
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error resetAllPieFolderSyncedFlags!", ex);
+		}
+	}
 
-    @Override
-    public void resetAllPieFolderSyncedFlags() {
-        try {
-            pieFilderDBService.resetAllPieFolderSyncedFlags();
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error resetAllPieFolderSyncedFlags!", ex);
-        }
-    }
-
-    @Override
-    public void removePieFolder(PieFolder folder) {
-        try {
-            pieFilderDBService.removePieFolder(folder);
-        } catch (SQLException ex) {
-            PieLogger.error(this.getClass(), "Error delete pieFolder!", ex);
-        }
-    }
+	@Override
+	public void removePieFolder(PieFolder folder) {
+		try {
+			pieFilderDBService.removePieFolder(folder);
+		} catch (SQLException ex) {
+			PieLogger.error(this.getClass(), "Error delete pieFolder!", ex);
+		}
+	}
 }
